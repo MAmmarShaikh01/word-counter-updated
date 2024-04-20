b7dbbc3308b725026c21e2bdf00e309b7b388ce0
jhukegaNahiSaala <shaikhurwa61@gmail.com>
2024-04-20 01:55:37 -0700

    word-counter

---
+++ index.js
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+let condition = true;
+let answer;
+while (condition) {
+    answer = await inquirer.prompt([{
+            name: "sentence",
+            type: "input",
+            message: chalk.red("Enter your sentence: ")
+        }]);
+    if (answer.sentence !== '')
+        condition = false;
+}
+let sep = answer.sentence.split(" ");
+let newArr = [];
+sep.forEach((word) => {
+    if (word != "") {
+        newArr.push(word);
+    }
+});
+if (newArr.length === 1) {
+    console.log(chalk.green(`there is only ${newArr.length} word in the sentence`));
+}
+else {
+    console.log(chalk.green(`there are ${newArr.length} words in the sentence`));
+}
